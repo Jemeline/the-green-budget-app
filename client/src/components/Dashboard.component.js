6b74eb805f57eddea12dfb744cba88fe1883d98e
Sami Habib <46608419+samih36@users.noreply.github.com>
2020-10-28 17:48:39 -0400

    tiny fix with className

--- client/src/components/Dashboard.component.js
+++ client/src/components/Dashboard.component.js
@@ -28,19 +28,19 @@ class Dashboard extends Component {
             <div>
                 <FakeHeader className={'Header'} handleLogout={this.handleLogout}/>
             </div>
-            <div class="row">
+            <div className="row">
             <div className={"column left"}>
                 <Widget direction={'left'} text={'+'} />
                 <Widget direction={'left'} text={'-'}/>
                 <Widget direction={'left'} text={'edit'}/>
             </div>
-            <div class="column middle">
+            <div className="column middle">
                 
                 <div className={'Chart'}>
                 <PieChart className={'Chart'}/>
                 </div>
             </div>
-            <div class="column right">
+            <div className="column right">
                 <Widget direction={'right'} text={'stocks'} />
                 <Widget direction={'right'} text={'check my progress'}/>
                 <Widget direction={'right'} text={'impact'}/>
