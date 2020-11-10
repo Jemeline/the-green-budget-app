e2ca6e4970912d00bce56260f39295c0ac8c51cc
Jada Pfeiffer <jadapfeiffer@Jadas-MacBook-Pro.local>
2020-11-09 22:54:30 -0500

    final heroku test with budget component

--- client/src/utils/apiCalls.js
+++ client/src/utils/apiCalls.js
@@ -46,7 +46,7 @@ export async function addBudgetItem(data, headers) {
 
 export async function removeBudgetItem(data, headers) {
     try {
-        const res = await axios.post('https://green-budget-app.herokuapp.com/api/expenses/create-expense', data, headers);
+        const res = await axios.post('https://green-budget-app.herokuapp.com/api/expenses/delete-expense', data, headers);
         // const res = await axios.post('http://localhost:5000/api/expenses/delete-expense', data, headers);
         return await res;
     } catch (error){
@@ -56,7 +56,7 @@ export async function removeBudgetItem(data, headers) {
 };
 export async function updateBudgetItem(data, headers) {
     try {
-        const res = await axios.post('https://green-budget-app.herokuapp.com/api/expenses/create-expense', data, headers);
+        const res = await axios.post('https://green-budget-app.herokuapp.com/api/expenses/update-expense', data, headers);
         // const res = await axios.post('http://localhost:5000/api/expenses/update-expense', data, headers);
         return await res;
     } catch (error){
@@ -67,7 +67,7 @@ export async function updateBudgetItem(data, headers) {
 
 export async function getBudgetData(data, headers) {
     try {
-        const res = await axios.post('https://green-budget-app.herokuapp.com/api/expenses/create-expense', data, headers);
+        const res = await axios.post('https://green-budget-app.herokuapp.com/api/expenses/read-expenses', data, headers);
         // const res = await axios.post('http://localhost:5000/api/expenses/read-expenses', data, headers);
         return await res.data.data;
     } catch (error){
